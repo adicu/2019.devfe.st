9277a86765a0a136dfb5743b4490662f8d5d8629
Melanie Sawyer <melaniensawyer@gmail.com>
2019-01-25 19:44:37 -0500

    make the animation do the thing

--- dist/js/prizes-animation.js
+++ dist/js/prizes-animation.js
@@ -19,7 +19,8 @@ $(document).ready(function() {
       $("#red-2").mouseover(function() {
         console.log("Starting hover function");
           $("#red-rect-2").animate({
-            width: "500px"
+            width: "655px",
+              "margin-left": "-225px"
           }, 1000, function() {
           });
       });
@@ -27,7 +28,8 @@ $(document).ready(function() {
       $("#red-2").mouseout(function() {
         console.log("Starting mouseout function");
           $("#red-rect-2").animate({
-            width: "170px"
+            width: "170px",
+              "margin-left": "0px"
           }, 1000, function() {
           });
       });
